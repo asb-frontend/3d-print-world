b2c9465fda65639b7c6fb9fa1e405e1883c65e2a
Andrew S Baker <shakenbaker242@gmail.com>
2024-06-16 23:14:21 -0500

    adding fix

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import NavBar from "./components/Navbar/Navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar/>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
             {/* <Route path="/contact" component={Contact} /> */}
             {/* Add more routes for additional pages */}
             {/* <Route path="/newpage" component={NewPage} /> */}
-          </Switch>
-        <Home />
+          </Routes>
       </Router>
     </div>
   );
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,10 @@
 import React from "react";
-import s from "./Home.module.css";
+import s from "./About.module.css";
 
 const About = () => {
  
   return (
-    <div className={s.home}>
+    <div className={s.about}>
       <h1>Welcome to About Page</h1>
     </div>
   );
