72424f3f07efccdf873262bbf14e05800d37d56b
Andrew S Baker <shakenbaker242@gmail.com>
2024-06-19 20:47:53 -0500

    add appwrite and login

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import NavBar from "./components/Navbar/Navbar";
 import Store from "./components/Store/Store";
+import Login from "./components/Login/Login"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Client } from 'appwrite';
 
 function App() {
+  const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('667375240009c7efa89e');
+
   return (
     <div className="App">
       <Router>
@@ -14,7 +22,7 @@ function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>} />
             <Route path="/store" element={<Store/>} />
-          
+            <Route path="/login" element={<Login/>} />
           </Routes>
       </Router>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.css";
+import image1 from "../../images/Logo.jpg";
 import { CodeIcon, HamburgetMenuClose, HamburgetMenuOpen } from "./Icons";
 
 function NavBar() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
-  
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
   return (
     <>
       <nav className={s.navbar}>
         <div className={s.navContainer}>
           <NavLink exact to="/" className={s.navLogo}>
+            <img className={s.logo} src={image1} />
             <span>AK-Printing</span>
-            <span className={s.icon}>
-            </span>
+            <span className={s.icon}></span>
           </NavLink>
 
           <ul className={click ? `${s.navMenu} ${s.active}` : s.navMenu}>
@@ -51,18 +56,25 @@ function NavBar() {
                 Store
               </NavLink>
             </li>
-            {/* <li className={s.navItem}>
-              <NavLink
-                exact
-                to="/contact"
-                activeClassName={s.active}
-                className={s.navLinks}
-                onClick={handleClick}
-              >
-                Contact Us
-              </NavLink>
-            </li> */}
           </ul>
+          {!loggedInUser ? (
+            <div className={s.navLoginContainer}>
+              <div className={s.navLogin}>
+                <NavLink
+                  exact
+                  to="/login"
+                  activeClassName={s.active}
+                  className={s.navLinks}
+                  onClick={handleClick}
+                >
+                  Login
+                </NavLink>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+
           <div className={s.navIcon} onClick={handleClick}>
             {click ? (
               <span className={s.icon}>
